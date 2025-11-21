df97b449ba45a782fc74b41d1624887749957720
Aditya Putra <adityputr45@gmail.com>
2025-11-21 19:09:31 +0700

    bring back hamburger

--- js/script.js
+++ js/script.js
@@ -8,6 +8,12 @@ window.addEventListener("load", function () {
     document.getElementById("welcome").innerText = `Hi ${name}! Welcome to My Website`;
 });
 
+// Hamburger
+function toggleMenu() {
+    document.querySelector("nav ul").classList.toggle("show");
+}
+
+
 // Form Validation + Print result
 function submitForm() {
     let name = document.getElementById("name").value.trim();
